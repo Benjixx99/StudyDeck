ebf382a6ff22e3d7d15876a39c3d36d9309da65f
Benjixx99 <anon_writer@startmail.com>
2025-11-09 21:52:15 +0100

    feat (data): add interface BaseDao

---
+++ data/src/main/java/bx/app/data/local/dao/BaseDao.kt
@@ -0,0 +1,24 @@
+package bx.app.data.local.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+internal interface BaseDao<T> {
+    fun observeAll(): Flow<List<T>>
+
+    suspend fun getById(id: Long): T
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entity: T): Long
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(entity: T)
+
+    @Delete
+    suspend fun delete(entity: T)
+
+    suspend fun deleteAll()
+}
