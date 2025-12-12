4983319d55c6eb9ad6d07eab180caf63a2adfb8b
Benjixx99 <anon_writer@startmail.com>
2025-12-12 21:49:39 +0100

    test (data):  add some database mock data

--- data/src/main/java/bx/app/data/local/mock/DatabaseMockData.kt
+++ data/src/main/java/bx/app/data/local/mock/DatabaseMockData.kt
@@ -1,10 +1,13 @@
 package bx.app.data.local.mock
 
 import bx.app.data.enums.CardFailing
+import bx.app.data.enums.CardSide
 import bx.app.data.enums.CardSideType
+import bx.app.data.model.AudioSideModel
 import bx.app.data.model.CardModel
 import bx.app.data.model.DeckModel
 import bx.app.data.model.LevelModel
+import bx.app.data.model.TextSideModel
 
 object DatabaseMockData {
     val decks = listOf<DeckModel>(
@@ -56,8 +59,63 @@ object DatabaseMockData {
             frontSideType = CardSideType.TEXT,
             frontSideId = 7,
             backSideType = CardSideType.AUDIO,
-            backSideId = 8,
-            deckId = 1,
+            backSideId = 1,
+            deckId = 2,
+        )
+    )
+
+    val textSide = listOf<TextSideModel>(
+        TextSideModel(
+            id = 1,
+            text = "Boy",
+            side = CardSide.FRONT,
+            cardId = 1
+        ),
+        TextSideModel(
+            id = 2,
+            text = "Junge",
+            side = CardSide.BACK,
+            cardId = 1
+        ),
+        TextSideModel(
+            id = 3,
+            text = "Girl",
+            side = CardSide.FRONT,
+            cardId = 2
+        ),
+        TextSideModel(
+            id = 4,
+            text = "Mädchen",
+            side = CardSide.BACK,
+            cardId = 2
+        ),
+        TextSideModel(
+            id = 5,
+            text = "incredible",
+            side = CardSide.FRONT,
+            cardId = 3
+        ),
+        TextSideModel(
+            id = 6,
+            text = "unglaublich",
+            side = CardSide.BACK,
+            cardId = 3
+        ),
+        TextSideModel(
+            id = 7,
+            text = "You can do it",
+            side = CardSide.FRONT,
+            cardId = 4
+        )
+    )
+
+    val audioSide = listOf<AudioSideModel>(
+        AudioSideModel(
+            id = 1,
+            fileName = "Test Audio",
+            path = "test/path",
+            side = CardSide.BACK,
+            cardId = 3
         )
     )
 
