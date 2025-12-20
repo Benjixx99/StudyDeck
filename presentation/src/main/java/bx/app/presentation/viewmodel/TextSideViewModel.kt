05dc7eecfd9837ed0c2a11484570c23ff09366b3
Benjixx99 <anon_writer@startmail.com>
2025-12-20 13:20:21 +0100

    feat: add debounce and upsert to text side

--- presentation/src/main/java/bx/app/presentation/viewmodel/TextSideViewModel.kt
+++ presentation/src/main/java/bx/app/presentation/viewmodel/TextSideViewModel.kt
@@ -1,6 +1,5 @@
 package bx.app.presentation.viewmodel
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import bx.app.data.enums.CardSide
 import bx.app.data.model.TextSideModel
@@ -11,13 +10,9 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class TextSideViewModel(private val repo: TextSideRepository) : ViewModel() {
+class TextSideViewModel(private val repo: TextSideRepository) : DebouncedAutoSaveViewModel() {
     private val _textById = MutableStateFlow<Map<Long, String?>>(emptyMap())
-    private var _textSide = MutableStateFlow<TextSideModel>(TextSideModel(
-        text = "",
-        side = CardSide.FRONT,
-        cardId = 0
-    ))
+    private var _textSide = MutableStateFlow<TextSideModel>(getInitialTextSide())
 
     val textById: StateFlow<Map<Long, String?>> = _textById
     var textSide: StateFlow<TextSideModel> = _textSide
@@ -33,4 +28,19 @@ class TextSideViewModel(private val repo: TextSideRepository) : ViewModel() {
     fun insertTextSide(textSide: TextSideModel) = viewModelScope.launch { repo.insert(textSide) }
     fun updateTextSide(textSide: TextSideModel) = viewModelScope.launch { repo.update(textSide) }
     fun deleteTextSide(textSide: TextSideModel) = viewModelScope.launch { repo.delete(textSide) }
+
+    fun changeText(value: String) = upsertTextSide { _textSide.value.copy(text = value) }
+
+    private fun upsertTextSide(transform: TextSideModel.() -> TextSideModel) {
+        _textSide.value = _textSide.value.transform()
+        scheduleAutoSave { getTextSideById(repo.upsert(_textSide.value)) }
+    }
+
+    private fun getInitialTextSide(): TextSideModel {
+        return TextSideModel(
+            text = "",
+            side = CardSide.FRONT,
+            cardId = 0
+        )
+    }
 }
