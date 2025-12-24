2ac55853f8794aa8356bd868e5de5b28773c0785
Benjixx99 <anon_writer@startmail.com>
2025-12-24 14:23:30 +0100

    refactor (presentation): improve AudioSideViewModel
    add resetAudioSide and getAudioSideByCard
    remove debounce functionality

--- presentation/src/main/java/bx/app/presentation/viewmodel/AudioSideViewModel.kt
+++ presentation/src/main/java/bx/app/presentation/viewmodel/AudioSideViewModel.kt
@@ -1,9 +1,13 @@
 package bx.app.presentation.viewmodel
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import bx.app.data.enums.CardSide
+import bx.app.data.enums.CardSideType
 import bx.app.data.model.AudioSideModel
+import bx.app.data.model.CardModel
 import bx.app.data.repository.AudioSideRepository
+import bx.app.presentation.data.IdValidator
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
@@ -11,7 +15,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlin.collections.plus
 
-class AudioSideViewModel(private val repo: AudioSideRepository) : DebouncedAutoSaveViewModel() {
+class AudioSideViewModel(private val repo: AudioSideRepository) : ViewModel() {
     private val _fileNameById = MutableStateFlow<Map<Long, String?>>(emptyMap())
     private var _audioSide = MutableStateFlow<AudioSideModel>(getInitialAudioSide())
 
@@ -19,6 +23,17 @@ class AudioSideViewModel(private val repo: AudioSideRepository) : DebouncedAutoS
     var audioSide: StateFlow<AudioSideModel> = _audioSide
 
     fun getAudioSideById(id: Long) = viewModelScope.launch { _audioSide.value = repo.getById(id) }
+
+    fun getAudioSideByCard(card: CardModel, cardSide: CardSide) {
+        val id =
+            if (cardSide == CardSide.FRONT)
+                card.frontSideId.takeIf { card.frontSideType == CardSideType.AUDIO }
+            else
+                card.backSideId.takeIf { card.backSideType == CardSideType.AUDIO }
+
+        if (id != null && id >= IdValidator.MIN_VALID_ID) getAudioSideById(id)
+    }
+
     fun getFileNameById(id: Long) = viewModelScope.launch {
         if (_fileNameById.value.containsKey(id)) return@launch
         repo.getFileNameById(id).filterNotNull().collect {
@@ -30,12 +45,10 @@ class AudioSideViewModel(private val repo: AudioSideRepository) : DebouncedAutoS
     fun updateAudioSide(audioSide: AudioSideModel) = viewModelScope.launch { repo.update(audioSide) }
     fun deleteAudioSide(audioSide: AudioSideModel) = viewModelScope.launch { repo.delete(audioSide) }
 
-    fun changeAudioData(path: String, fileName: String) =
-        upsertAudioSide { _audioSide.value.copy(path = path, fileName = fileName) }
-
-    private fun upsertAudioSide(transform: AudioSideModel.() -> AudioSideModel) {
-        _audioSide.value = _audioSide.value.transform()
-        scheduleAutoSave { getAudioSideById(repo.upsert(_audioSide.value)) }
+    fun resetAudioSide() { _audioSide.value = getInitialAudioSide() }
+    fun resetAudioSide(audioSide: AudioSideModel) {
+        resetAudioSide()
+        _audioSide.value = audioSide
     }
 
     private fun getInitialAudioSide(): AudioSideModel {
