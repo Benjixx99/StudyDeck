385248324295549eb601387aceca1968182bee3a
Benjixx99 <anon_writer@startmail.com>
2026-01-01 15:17:57 +0100

    refactor (data): add foreign keys to CardInLevelEntity

--- data/src/main/java/bx/app/data/local/entity/CardInLevelEntity.kt
+++ data/src/main/java/bx/app/data/local/entity/CardInLevelEntity.kt
@@ -2,6 +2,7 @@ package bx.app.data.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.TypeConverters
 import bx.app.data.converter.LocalDateTimeConverter
@@ -11,6 +12,19 @@ import java.time.LocalDateTime
 @Entity(
     tableName = "card_in_level",
     primaryKeys = ["card_id", "level_id"],
+    foreignKeys = [
+        ForeignKey(
+            entity = CardEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["card_id"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = LevelEntity::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("level_id"),
+            onDelete = ForeignKey.CASCADE
+    )],
     indices = [Index(value = ["card_id"], unique = true)]
 )
 @TypeConverters(LocalDateTimeConverter::class)
