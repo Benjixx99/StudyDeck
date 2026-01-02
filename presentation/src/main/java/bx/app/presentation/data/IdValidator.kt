3714a5ff9bc49f7b1249177346c99d2f2f91a249
Benjixx99 <anon_writer@startmail.com>
2026-01-02 13:27:05 +0100

    refactor (presentation): remove unused const variable and useless todo

--- presentation/src/main/java/bx/app/presentation/data/IdValidator.kt
+++ presentation/src/main/java/bx/app/presentation/data/IdValidator.kt
@@ -2,6 +2,5 @@ package bx.app.presentation.data
 
 object IdValidator {
     const val INSERT = -1L
-    const val UPDATE = -2L
     const val MIN_VALID_ID = 1L
 }
--- presentation/src/main/java/bx/app/presentation/viewmodel/HideNavigationBarViewModel.kt
+++ presentation/src/main/java/bx/app/presentation/viewmodel/HideNavigationBarViewModel.kt
@@ -5,8 +5,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-// TODO: Need to change the name because its task is to hide the navigation bar and i need this for the bug [0002] too
-// but it hides the nav bar if (deckId <= IdValidator.MIN_VALUE)
 class HideNavigationBarViewModel : ViewModel() {
     private val _hide = MutableStateFlow(false)
     val hide: StateFlow<Boolean> = _hide.asStateFlow()
