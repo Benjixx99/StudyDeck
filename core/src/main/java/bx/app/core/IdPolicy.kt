1ad1d9794ef78862edc679da85dfb57fa07ab7f4
Benjixx99 <anon_writer@startmail.com>
2026-01-17 23:37:09 +0100

    feat (core): add functions and const variables

--- core/src/main/java/bx/app/core/IdPolicy.kt
+++ core/src/main/java/bx/app/core/IdPolicy.kt
@@ -3,6 +3,10 @@ package bx.app.core
 object IdPolicy {
     internal const val MIN_VALID_ID = 1L
     internal const val INSERT = -1L
+    internal const val RANDOM_ID = -10L
+    internal const val LEVEL_BASED_ID = -11L
 
     fun getInsertId(): Long = INSERT
+    fun getRandomId(): Long = RANDOM_ID
+    fun getLevelBasedId(): Long = LEVEL_BASED_ID
 }
--- core/src/main/java/bx/app/core/LongExtension.kt
+++ core/src/main/java/bx/app/core/LongExtension.kt
@@ -2,4 +2,5 @@ package bx.app.core
 
 fun Long.hasInvalidId(): Boolean = this.toLong() < IdPolicy.MIN_VALID_ID
 fun Long.hasValidId(): Boolean = this.toLong() >= IdPolicy.MIN_VALID_ID
-fun Long.isInsert(): Boolean = this.toLong() == IdPolicy.INSERT
+fun Long.hasInsertId(): Boolean = this.toLong() == IdPolicy.INSERT
+fun Long.hasRandomId(): Boolean = this.toLong() == IdPolicy.RANDOM_ID
