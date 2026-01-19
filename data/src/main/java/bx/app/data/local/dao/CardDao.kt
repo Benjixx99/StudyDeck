533bbd66d02236862f077114f02a787de78de5aa
Benjixx99 <anon_writer@startmail.com>
2026-01-19 20:56:58 +0100

    feat (data): integrate interval filter

--- data/src/main/java/bx/app/data/local/dao/CardDao.kt
+++ data/src/main/java/bx/app/data/local/dao/CardDao.kt
@@ -17,7 +17,13 @@ internal interface CardDao : BaseDao<CardEntity> {
         SELECT c.* 
         FROM card AS c
         JOIN card_in_level AS cil ON cil.card_id = c.id
-        WHERE cil.level_id = :id
+        JOIN level AS l ON l.id = cil.level_id
+        WHERE cil.level_id = :id 
+        AND (
+            cil.last_time_learned_date IS NULL
+            OR (strftime('%s','now') - (cil.last_time_learned_date / 1000.0)) 
+                / 86400.0 >= l.interval_number * l.interval_type
+        )
     """)
     fun observeByLevelId(id: Long): Flow<List<CardEntity>>
 
