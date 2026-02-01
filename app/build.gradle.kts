7ed4c3f64edbbca27f5b0f51e562703703bb9b72
Benjixx99 <anon_writer@startmail.com>
2026-02-01 21:18:37 +0100

    build (gradle): bump version to 1.0.0-beta-1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1.0 - Beta"
+        versionName = "1.0.0-beta.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
