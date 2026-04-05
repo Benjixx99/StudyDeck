ea870a5ada30716a6de5b605b4162ea7a7af7559
Benjixx99 <anon_writer@startmail.com>
2026-04-06 01:15:51 +0200

    feat (presentation): add cardsSortMode flow to TopBarViewModel

--- presentation/src/main/java/bx/app/presentation/viewmodel/TopBarViewModel.kt
+++ presentation/src/main/java/bx/app/presentation/viewmodel/TopBarViewModel.kt
@@ -1,13 +1,17 @@
 package bx.app.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import bx.app.data.enums.SortMode
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class TopBarViewModel : ViewModel() {
     private val _title = MutableStateFlow("")
+    private val _cardsSortMode = MutableStateFlow(SortMode.ID_ASC)
     val title: StateFlow<String> = _title.asStateFlow()
+    val cardsSortMode: StateFlow<SortMode> = _cardsSortMode.asStateFlow()
 
     fun setTitle(title: String) { _title.value = title }
+    fun setCardsSortMode(sortMode: SortMode) { _cardsSortMode.value = sortMode }
 }
